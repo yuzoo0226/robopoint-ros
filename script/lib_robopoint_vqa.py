ef1abc79994bd52a504e96f7abddfdc04fa323b1
yuzoo0226 <fryuzoo@gmail.com>
2025-07-01 01:29:59 +0900

    del some comments

--- script/lib_robopoint_vqa.py
+++ script/lib_robopoint_vqa.py
@@ -19,30 +19,6 @@ class GetPlacePoseClient:
         rospy.wait_for_service("/robopoint/get_place_pose")
         self.get_place_pose_service = rospy.ServiceProxy("/robopoint/get_place_pose", GetPlacePose)
 
-        # image_paths = sorted(glob.glob("./io/test_images/images/*.png"))
-        # mask_paths = sorted(glob.glob("./io/test_images/masks/*.png"))
-
-        # assert len(image_paths) == len(mask_paths), "Unmatched images and masks number"
-
-        # self.images = []
-        # self.masks = []
-
-        # for img_path, mask_path in zip(image_paths, mask_paths):
-        #     cv_img = cv2.imread(img_path)
-        #     cv_mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-
-        #     if cv_img is None or cv_mask is None:
-        #         rospy.logerr(f"Failed to load {img_path} or {mask_path}")
-        #         continue
-
-        #     cv_mask_rgb = cv2.cvtColor(cv_mask, cv2.COLOR_GRAY2RGB)
-
-        #     ros_img = self.bridge.cv2_to_imgmsg(cv_img, encoding="bgr8")
-        #     ros_mask = self.bridge.cv2_to_imgmsg(cv_mask_rgb, encoding="rgb8")
-
-        #     self.images.append(ros_img)
-        #     self.masks.append(ros_mask)
-
     def call_get_place_pose(self, image_path: str):
         """Call the get_place_pose service with the given image path.
 
